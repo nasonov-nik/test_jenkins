ed601a33b8bc3b75a2d4184959afbaac64175f09
nikita <nasonov.nik@gmail.com>
2023-02-13 17:16:56 +0300

    probe declarative.groovy

--- declarative.groovy
+++ declarative.groovy
@@ -14,16 +14,18 @@ pipeline {
 
             }
         }
-        stage("sh с сохранением"){
-            steps{
-                script {
-                    remote_host = sh (
-                            script: ' ssh -o StrictHostKeyChecking=no osboxes@192.168.59.102 \'hostname\'',
-                            returnStdout: true
-                    ).trim()
-                    println(remote_host)
-                }
+        withCredentials([sshUserPrivateKey(credentialsId: "yourkeyid", keyFileVariable: 'keyfile')]) {
+            stage("sh с сохранением"){
+                steps{
+                    script {
+                        remote_host = sh (
+                                script: ' ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'',
+                                returnStdout: true
+                        ).trim()
+                        println(remote_host)
+                    }
 
+                }
             }
         }
     }
--- script.groovy
+++ script.groovy
@@ -1,5 +1,4 @@
 node {
-
     stage("check host") {
         lol = sh(script: "ssh osboxes@192.168.59.102 \'hostname\'", returnStdOut: true)
         println(lol)
