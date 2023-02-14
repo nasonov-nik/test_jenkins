e54e0344417babff7ae6ff0541740827bcee8a3c
nikita <nasonov.nik@gmail.com>
2023-02-14 18:15:57 +0300

    probe jenkins

--- script.groovy
+++ script.groovy
@@ -1,6 +1,9 @@
 node {
     stage("check host") {
-        lol = sh(script: "ssh osboxes@192.168.59.102 \'hostname\'", returnStdOut: true)
-        println(lol)
+
+        withCredentials([sshUserPrivateKey(credentialsId: "osboxes", keyFileVariable: 'keyfile')]) {
+            lol = sh(script: "ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'", returnStdOut: true)
+            println(lol)
+        }
     }
 }
