13d3fc0afc28e0c4b8d07e9c25940676106a2bf7
nikita <nasonov.nik@gmail.com>
2023-03-18 15:54:20 +0300

    probe jenkins

--- script.groovy
+++ script.groovy
@@ -4,8 +4,13 @@ node {
         test2=readYaml text: env.test2
     }
     stage("check host") {
+        println(test1)
+        println(test2)
         println(test1.getClass())
         print(test2.getClass())
+        test1.each {x ->
+            println(x)
+        }
         withCredentials([sshUserPrivateKey(credentialsId: "osboxes", keyFileVariable: 'keyfile')]) {
             lol = sh(script: "ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'", returnStdout: true)
             println(lol)
