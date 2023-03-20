eab1041dca2f9b495b8ec2e4da64c71a1de41101
nikita <nasonov.nik@gmail.com>
2023-03-20 11:08:01 +0300

    probe jenkins

--- script.groovy
+++ script.groovy
@@ -1,15 +1,9 @@
 node {
     environment {
-        test1 = env.test1
-        test2 = env.test2
+        pwdEnv = pwd()
     }
     stage("check host") {
-        test1_list = test1.split(',')
-        test2_list = test2.split(',')
-        println(test1_list)
-        println(test2_list)
-        println(test1_list.getClass())
-        print(test2_list.getClass())
+        println(pwdEnv)
         withCredentials([sshUserPrivateKey(credentialsId: "osboxes", keyFileVariable: 'keyfile')]) {
             lol = sh(script: "ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'", returnStdout: true)
             println(lol)
--- test.groovy
+++ test.groovy
@@ -1,4 +1,7 @@
-def runtime_props = [:]
-runtime_props.put('lol', [:])
+//env1 = "privet"
+//def runtime_props = [:]
+//runtime_props.put('lol', env1 + "ghbdtn")
+//print(runtime_props)
 
-println(runtime_props.lol)
+
+lol = pwd()//?
