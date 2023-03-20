8b25bef2d4760f5bbb1797bbe8b02a1aaac62b3e
nikita <nasonov.nik@gmail.com>
2023-03-20 14:36:41 +0300

    probe jenkins

--- script.groovy
+++ script.groovy
@@ -1,9 +1,11 @@
 node {
     environment {
-        pwdEnv = $PWD
+        test1 = env.test1
+        test2 = env.test2
     }
     stage("check host") {
-        println(pwdEnv)
+        test1 = test1.each(',').toList()
+        println(test1)
         withCredentials([sshUserPrivateKey(credentialsId: "osboxes", keyFileVariable: 'keyfile')]) {
             lol = sh(script: "ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'", returnStdout: true)
             println(lol)
--- test.groovy
+++ test.groovy
@@ -1,7 +1,20 @@
-//env1 = "privet"
-//def runtime_props = [:]
-//runtime_props.put('lol', env1 + "ghbdtn")
-//print(runtime_props)
+dors = "df,dfdf,dfdf,dfdf,dfd,fdf"
+dors = dors.split(',')
+println(dors.toList())
+jobs = [:]
+jobs.put("first", "did")
+jobs.put("second", "dod")
 
+runtime_props = [:]
 
-lol = pwd()//?
+runtime_props.put("conf", [(jobs."first"): jobs."second"])
+
+println(runtime_props)
+
+//runtime_props.conf = [:]
+//runtime_props.conf.put("lol", null)
+//runtime_props.conf.put("lol2", "lolo")
+//if (!runtime_props.conf.lol) {
+//    runtime_props.conf.lol = "pampam"
+//}
+//println(runtime_props.conf.lol)
