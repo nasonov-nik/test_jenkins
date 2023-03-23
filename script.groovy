1c2b20b6df881827c3f5b6058211138c430bd0ce
nikita <nasonov.nik@gmail.com>
2023-03-23 09:37:49 +0300

    probe jenkins

--- script.groovy
+++ script.groovy
@@ -1,22 +1,20 @@
 node {
-    stages {
-        stage("find") {
-            script {
-                sh "mkdir -p pop/lol/tot"
-                sh "touch pop/lol/tot/lol.yaml"
-                def lol = findFiles(glob: '**/*.yaml')
-                for (file in lol) {
-                    println("lol ${file}")
-                }
+    stage("find") {
+        script {
+            sh "mkdir -p pop/lol/tot"
+            sh "touch pop/lol/tot/lol.yaml"
+            def lol = findFiles(glob: '**/*.yaml')
+            for (file in lol) {
+                println("lol ${file}")
             }
         }
-        stage("check host") {
-            steps {
-                script {
-                    withCredentials([sshUserPrivateKey(credentialsId: "osboxes", keyFileVariable: 'keyfile')]) {
-                        lol = sh(script: "ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'", returnStdout: true)
-                        println(lol)
-                    }
+    }
+    stage("check host") {
+        steps {
+            script {
+                withCredentials([sshUserPrivateKey(credentialsId: "osboxes", keyFileVariable: 'keyfile')]) {
+                    lol = sh(script: "ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'", returnStdout: true)
+                    println(lol)
                 }
             }
         }
