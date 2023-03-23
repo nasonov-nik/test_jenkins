7e4e3ff19915793dfd29520c7b4e5908b22bf9ec
nikita <nasonov.nik@gmail.com>
2023-03-23 09:08:57 +0300

    probe jenkins

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ node {
         steps{
             script{
                 sh "touch lol.yaml"
-                lol = findFiles(glob '**/*.yaml')
+                lol = findFiles(glob: '**/*.yaml')
                 println(lol)
                 withCredentials([sshUserPrivateKey(credentialsId: "osboxes", keyFileVariable: 'keyfile')]) {
                     lol = sh(script: "ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'", returnStdout: true)
