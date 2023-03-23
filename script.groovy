3717756ccd0b13fc6e2aefbe61741b99561bf24b
nikita <nasonov.nik@gmail.com>
2023-03-23 09:08:31 +0300

    probe jenkins

--- script.groovy
+++ script.groovy
@@ -4,14 +4,16 @@ node {
         test2 = env.test2
     }
     stage("check host") {
-        if (test1){
-            println("ok")
-        }else{
-            println("oops")
-        }
-        withCredentials([sshUserPrivateKey(credentialsId: "osboxes", keyFileVariable: 'keyfile')]) {
-            lol = sh(script: "ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'", returnStdout: true)
-            println(lol)
+        steps{
+            script{
+                sh "touch lol.yaml"
+                lol = findFiles(glob '**/*.yaml')
+                println(lol)
+                withCredentials([sshUserPrivateKey(credentialsId: "osboxes", keyFileVariable: 'keyfile')]) {
+                    lol = sh(script: "ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'", returnStdout: true)
+                    println(lol)
+                }
+            }
         }
     }
 }
--- test.groovy
+++ test.groovy
@@ -1,20 +1,8 @@
-dors = "df,dfdf,dfdf,dfdf,dfd,fdf"
-dors = dors.split(',')
-println(dors.toList())
-jobs = [:]
-jobs.put("first", "did")
-jobs.put("second", "dod")
+def lol = [name: "Jerry", age: 42, city: "New York"]
+def kek = [l1: "name", l2: "age"]
 
-runtime_props = [:]
 
-runtime_props.put("conf", [(jobs."first"): jobs."second"])
+kek.each {x, y ->
+    println(x)
 
-println(runtime_props)
-
-//runtime_props.conf = [:]
-//runtime_props.conf.put("lol", null)
-//runtime_props.conf.put("lol2", "lolo")
-//if (!runtime_props.conf.lol) {
-//    runtime_props.conf.lol = "pampam"
-//}
-//println(runtime_props.conf.lol)
+}
