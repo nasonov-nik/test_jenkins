4c018e0a8c449b1df92a6c412cd1709831cd7bb4
nikita <nasonov.nik@gmail.com>
2023-03-23 09:15:04 +0300

    probe jenkins

--- script.groovy
+++ script.groovy
@@ -1,17 +1,30 @@
+//node {
+//    stages {
+//        stage("check host") {
+//            steps {
+//                script {
+//                    sh "touch lol.yaml"
+//                    lol = findFiles(glob: '**/*.yaml')
+//                    println(lol)
+//                    withCredentials([sshUserPrivateKey(credentialsId: "osboxes", keyFileVariable: 'keyfile')]) {
+//                        lol = sh(script: "ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'", returnStdout: true)
+//                        println(lol)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
 node {
-    stages{
-        stage("check host") {
-            steps{
-                script{
-                    sh "touch lol.yaml"
-                    lol = findFiles(glob: '**/*.yaml')
-                    println(lol)
-                    withCredentials([sshUserPrivateKey(credentialsId: "osboxes", keyFileVariable: 'keyfile')]) {
-                        lol = sh(script: "ssh -o StrictHostKeyChecking=no -i ${keyfile} osboxes@192.168.59.102 \'hostname\'", returnStdout: true)
-                        println(lol)
-                    }
-                }
-            }
-        }
+
+    stage(find){
+        sh "touch lol.yaml"
+        lol = findFiles(glob: '**/*.yaml')
+        println(lol)
+    }
+    stage("check host") {
+        lol = sh(script: "ssh osboxes@192.168.59.102 \'hostname\'", returnStdOut: true)
+        println(lol)
     }
 }
