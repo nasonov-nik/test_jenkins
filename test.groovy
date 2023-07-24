a230173555ca82e423714a28c1da81d52a880fe8
nikita <mustang.boos@rambler.ru>
2023-07-24 12:01:59 +0300

    change test.groovy

--- test.groovy
+++ test.groovy
@@ -12,8 +12,11 @@ pipeline {
             steps {
                 script {
                     def timeYear = new Date().getYear() + 1900
-                    def timeMonth = new Date().getMonth()
+                    def timeMonth = new Date().getMonth() + 1
                     def timeDay = new Date().getDate()
+                    def timeHoures = new Date().getHours()
+                    def timeMinutes = new Date().getMinutes()
+                    def timeSeconds = new Date().getSeconds()
                     println([timeYear, timeMonth, timeDay])
                 }
             }
