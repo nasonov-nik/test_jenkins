4076f116f16f41261aa684bd8a008601b3bce221
nikita <mustang.boos@rambler.ru>
2023-11-04 16:15:29 +0300

    test

--- test.groovy
+++ test.groovy
@@ -11,16 +11,64 @@ pipeline {
         stage("Обработка параметров") {
             steps {
                 script {
-                    String template = readFile(file: 'template.yaml')
-                    def bindingFile = readFile(file: 'env.env')
-                    def binding = ['hosts':[:]]
+//                    String template = readFile(file: 'template.yaml')
+//                    def bindingFile = readFile(file: 'env.env')
+//                    def binding = ['hosts':[:]]
+//
+//                    bindingFile.split(',').each { line ->
+//                        lineSplit = line.split(':')
+//                        binding['hosts'].put(lineSplit[0].trim(),  lineSplit[1].trim())
+//                    }
+//                    println(binding)
 
-                    bindingFile.split(',').each { line ->
-                        lineSplit = line.split(':')
-                        binding['hosts'].put(lineSplit[0].trim(),  lineSplit[1].trim())
-                    }
-                    println(binding)
-                    template(binding, template)
+
+                    def binding = [
+                            dockerBuild: {
+                                true
+                            },
+                            version: 'v1',
+                            kind: 'Pod',
+                            labels:[
+                                    type: 'ephemeral-jenkins-agent',
+                                    pipeline: 'generic_pipeline'
+                            ]
+                    ]
+
+                    def template =     '''\
+                                        apiVersion: ${version}
+                                        kind: <%= kind %>
+                                        metadata:
+                                          labels:
+                                        <% for(label in labels){ %>\
+                                            ${label.key}: ${label.value}
+                                        <% } %>\
+                                        spec:
+                                          containers:
+                                          - name: alpine
+                                            image: alpine:3.12.3
+                                            command:
+                                            - cat
+                                            tty: true
+                                        <% if( dockerBuild() ){ %>\
+                                          - name: docker
+                                            image: docker:18.05-dind
+                                            securityContext:
+                                              privileged: true
+                                            volumeMounts:
+                                            - name: dind-storage
+                                              mountPath: /var/lib/docker
+                                        <% } %>\
+                                        <% if( dockerBuild() ){ %>\
+                                          volumes:
+                                            - name: dind-storage
+                                              emptyDir: {}
+                                        <% } else { %>\
+                                          volumes: {}
+                                        <% } %>\
+                                            '''
+
+                    println renderTemplate(binding,template)
+
                 }
             }
         }
