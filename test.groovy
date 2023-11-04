b63c232326702962b6447d42fcf2a881c9a8488d
nikita <mustang.boos@rambler.ru>
2023-11-04 15:51:58 +0300

    test

--- test.groovy
+++ test.groovy
@@ -16,7 +16,7 @@ pipeline {
 
                     bindingFile.split(',').each { line ->
                         lineSplit = line.split(':')
-                        println(Arrays.toString((lineSplit))
+                        println(Arrays.toString(lineSplit))
 //                        binding.put(lineSplit[0].trim(): lineSplit[1].trim())
                     }
                 }
