3e1121e060f68602cf3cf4b3ad175b479f54975a
nikita <mustang.boos@rambler.ru>
2023-11-04 15:42:52 +0300

    test

--- test.groovy
+++ test.groovy
@@ -18,7 +18,7 @@ pipeline {
                         println(line)
                         lineSplit = line.split(':')
                         println(lineSplit)
-                        binding.put(lineSplit[0]: lineSplit[1])
+                        binding.put(lineSplit[0].trim(): lineSplit[1].trim())
                     }
                 }
             }
