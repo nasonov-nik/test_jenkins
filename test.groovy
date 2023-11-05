c57552c1c271363b1feee75eb3e80ef3a9475226
nikita <mustang.boos@rambler.ru>
2023-11-05 18:30:46 +0300

    test

--- test.groovy
+++ test.groovy
@@ -67,7 +67,6 @@ pipeline {
                                           volumes: {}
                                         <% } %>\
                                             '''
-                    @NonCPS
                     def lol = renderTemplate(binding,template)
                     println(lol)
 //                }
